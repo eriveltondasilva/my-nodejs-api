348214ba6a9a9f76a52696a7c376f7d41a8273e2
Erivleton Silva <eriveltondasilva13@gmail.com>
2024-03-17 08:17:10 -0300

    adiciona packeges de segurança

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,14 @@
 import { json } from 'express'
+import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet'
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes).
+})
 
 export default function middleware(app) {
   app.use(json())
+  app.use(helmet())
+  app.use(limiter)
 }
